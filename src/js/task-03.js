e7409cb29882ea5a76d2c21115e2ba62944533fc
Mary Danyliuk <marydanyliuk@gmail.com>
2022-10-21 14:10:23 +0300

    task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -16,51 +16,11 @@ const images = [
 const galleryList = document.querySelector(`.gallery`);
 console.log(galleryList);
 
-// const galleryItemEl = document.createElement(`li`);
-// galleryItemEl.classList.add(`item`);
-// const newList = [];
+const galleryImgEl = images
+  .map(
+    (img) =>
+      `<li class="item"><img alt="${img.alt}" src="${img.url}" width=400></img></li>`
+  )
+  .join(` `);
 
-// const galleryItemEl = images.forEach((element) => {
-//   const galleryItemEl = document.createElement(`li`);
-//   galleryItemEl.classList.add(`item`);
-//   galleryItemEl.textContent = element;
-//   console.log(galleryItemEl);
-//   newList.push(galleryItemEl);
-//   galleryList.insertAdjacentHTML("afterbegin", galleryItemEl);
-// });
-
-// galleryList.insertAdjacentHTML("afterbegin", galleryItemEl);
-
-// const newList1 = [];
-1;
-const galleryImgEl = images.forEach((img) => {
-  const galleryImgEl = images.map(
-    (img) => `<li class="item"><img>${img}</img></li>`
-  );
-  galleryList.insertAdjacentHTML("afterbegin", galleryImgEl);
-});
-// 2.
-// const galleryImgEl = images.forEach((img) => {
-//   const galleryImgEl = images.map(
-//     (img) => `<li class="item"><img>${img}</img></li>`
-//   );
-//   galleryList.insertAdjacentHTML("afterbegin", galleryImgEl);
-// });
-
-// console.log(newList);
-
-// const galleryItem = document.createElement(`li`);
-// galleryItem.classList.add(`img`);
-
-// const galleryPic = document.createElement(`img`);
-// console.log(galleryPic);
-
-const markup = images.map((img) => `<li class="item"><img>${img}</img></li>`);
-console.log(markup);
-
-// galleryList.append(...newList);
-// galleryItem.append(markup);
-// .join(" ")
-// galleryList.insertAdjacentHTML("afterbegin", newList);
-
-// `<li class="item"><img>${img}</img></li>`;
+galleryList.insertAdjacentHTML("afterbegin", galleryImgEl);
