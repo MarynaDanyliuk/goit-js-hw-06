28b3a14e8cd4caa2df40c4c8213ce7c98b1944bf
Mary Danyliuk <marydanyliuk@gmail.com>
2022-10-22 10:04:18 +0300

    re-add start files

---
+++ js/task-01.js
@@ -0,0 +1,24 @@
+const categories = document.querySelectorAll(`#categories > .item`);
+console.log(`Number of categories: `, categories.length);
+
+// const categoriesEl = categories.forEach((element) => {
+//   console.log(
+//     `Category: `,
+//     element.querySelector(`#categories > .item > h2`).textContent
+//   );
+//   console.log(
+//     `Elements: `,
+//     element.querySelectorAll(`#categories > .item > ul > li`).length
+//   );
+// });
+
+const categoriesEl = categories.forEach((element) => {
+  console.log(
+    `Category: `,
+    element.querySelector(`#categories > .item > h2`).textContent
+  );
+  console.log(
+    `Elements: `,
+    element.querySelectorAll(`#categories > .item > ul > li`).length
+  );
+});
