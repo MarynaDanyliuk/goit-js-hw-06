61b9729040d1ede49ee56236a7559a6e47e5ab39
Mary Danyliuk <marydanyliuk@gmail.com>
2022-10-24 12:23:05 +0300

    task-04.js well done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,42 +1,23 @@
 const btnAddEl = document.querySelector('[data-action="increment"]');
 const btnRemoveEl = document.querySelector('[data-action="decrement"]');
-let counterValue = document.querySelector("#value");
+
+let counterValue = 0;
 
 console.log(counterValue);
 
 btnRemoveEl.addEventListener(`click`, () => {
-  console.log(`снимаю 1`);
-  counterValue.innerHTML -= 1;
+  counterValue -= 1;
+  console.log(`снимаю 1`, counterValue);
+  counterValueEl.textContent = counterValue;
 });
 
 btnAddEl.addEventListener(`click`, add1);
 
 function add1() {
-  counterValue.innerHTML += 1;
-  //   counterValue.insertAdjacentHTML("afterbegin", 1);
-  console.log(`добавляю 1`);
+  counterValue += 1;
+  console.log(`добавляю 1`, counterValue);
+  counterValueEl.textContent = counterValue;
 }
 
-// btnAddEl.addEventListener(`click`, () => {
-//   console.log(`снимаю 1`);
-//   btnRemoveEl.removeEventListener(`click`, remove1);
-// });
-
-// function remove1() {
-//   counterValue.innerHTML -= 1;
-//   console.log(`снимаю 1`);
-// }
-
-// btnAddEl.addEventListener(`click`, () => {
-//   console.log(`вешаю слушателя`);
-//   counterValue.innerHTML += 1;
-//   //   counterValue.insertAdjacentHTML("beforeBegin", Number(+1));
-//   //   counterValue.addEventListener(`click`, () => {});
-// });
-
-// function onBtnAddClick() {
-//   console.log(`вешаю слушателя`);
-// }
-// function onBtnRemoveClick() {
-//   console.log(-1);
-// }
+const counterValueEl = document.querySelector("#value");
+console.log(counterValueEl.textContent);
