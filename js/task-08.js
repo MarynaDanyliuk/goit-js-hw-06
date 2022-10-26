5b5adbbbeef05af41711ad521ff15a910d834907
Mary Danyliuk <marydanyliuk@gmail.com>
2022-10-26 23:57:53 +0300

    task-08.js done

--- js/task-08.js
+++ js/task-08.js
@@ -1,27 +1,23 @@
 const refs = {
   form: document.querySelector(`form`),
-  input: document.querySelector(`input`),
-  label: document.querySelector(`label`),
-  button: document.querySelector(`button`),
 };
 
 console.log(refs.form);
-console.log(refs.input);
-console.log(refs.label);
-console.log(refs.button);
 
 refs.form.addEventListener(`submit`, onFormSubmit);
-refs.input.addEventListener(`focus`, onInputFocus);
-refs.input.addEventListener(`blur`, onInputBlur);
-refs.input.addEventListener(`blur`, onInputChange);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(`Сабмит формы`);
-  console.log(event);
+
+  //   const formData = new FormData(event.currentTarget);
+
+  //   formData.forEach((value, name) => {
+  //     console.log(name);
+  //     console.log(value);
+  //   });
 
   const formElements = event.currentTarget.elements;
-  console.log(formElements);
+
   const email = formElements.email.value;
   const password = formElements.password.value;
 
@@ -30,33 +26,9 @@ function onFormSubmit(event) {
     password: password,
   };
 
-  console.log(formData);
-}
+  if (formData.mail === `` || formData.password === ``) {
+    alert("Заповніть будь ласка форму!");
+  } else console.log(formData);
 
-function onInputFocus() {
-  console.log(`инпут получил фокус`);
+  refs.form.reset();
 }
-function onInputBlur() {
-  console.log(`инпут потерял фокус`);
-}
-function onInputChange(event) {
-  event.preventDefault();
-  //   if (event.currentTarget.value.length <= 6) {
-  //     refs.input.setAttribute("class", "valid");
-  //     return console.log(event.currentTarget.value);
-  //   } else refs.input.setAttribute("class", "invalid");
-  //   console.log(refs.input.placeholder);
-}
-
-refs.input.addEventListener("input", (event) => {
-  event.preventDefault();
-  console.dir(event.currentTarget.elements);
-});
-
-// inputEl.addEventListener("input", (event) => {
-//   event.preventDefault();
-//   console.log(`это сабмит инпута`);
-//   //   console.log(event.currentTarget);
-//   console.dir(event.currentTarget.value);
-//   outputEl.textContent = event.currentTarget.value;
-// });
