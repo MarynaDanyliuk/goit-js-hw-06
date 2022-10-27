2e8642a23f6449e8e149d28555c3ff3091000335
Mary Danyliuk <marydanyliuk@gmail.com>
2022-10-27 21:27:00 +0300

    task-09.js done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  background: document.querySelector(`.widget`),
+  nameColor: document.querySelector(`.color`),
+  button: document.querySelector(`.change-color`),
+  body: document.querySelector(`body`),
+};
+console.log(refs.nameColor);
+console.log(refs.body);
+
+refs.button.addEventListener(`click`, onButtonClick);
+
+function onButtonClick() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.nameColor.textContent = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+  console.log(`клик на кнопку`);
+}
+
+// refs.nameColor.insertAdjacentHTML(
+//   "afterbegin",
+//   `${refs.nameColor.textContent}`
+// );
